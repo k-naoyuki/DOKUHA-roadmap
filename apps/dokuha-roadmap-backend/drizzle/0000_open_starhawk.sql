575c85947680cd5ac46d18989b68438aafa7ea62
ittyi <vioiv.k4@gmail.com>
2025-05-05 02:22:33 +0900

    🗃️ fix: カスタムSQLでやるとTSで利用したいモデルを作成したとき, CREATE文が二つできてしまったためもう一度やり直して手動でCREATE TRIGGERを追加

---
+++ apps/dokuha-roadmap-backend/drizzle/0000_open_starhawk.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`nickname` text NOT NULL,
+	`email` text NOT NULL,
+	`password` text NOT NULL,
+	`reading_mission` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
+
+-- NOTE: 以下のTRIGGERは、$onUpdate でうまく作成できず挫折したため手動で作成している
+CREATE TRIGGER `update_users_updated_at`
+AFTER UPDATE ON `users` FOR EACH ROW
+BEGIN
+    UPDATE `users` SET `updated_at` = CURRENT_TIMESTAMP WHERE `id` = OLD.`id`;
+END;
