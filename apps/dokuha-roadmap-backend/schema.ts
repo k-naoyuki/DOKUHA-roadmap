262a892b298fb069b3a5e650060895bffb83b15e
ittyi <vioiv.k4@gmail.com>
2025-05-06 09:01:00 +0900

    🗃️ feat: DBテーブル learning_contents を定義

--- apps/dokuha-roadmap-backend/schema.ts
+++ apps/dokuha-roadmap-backend/schema.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 export const users = sqliteTable(
@@ -11,6 +11,23 @@ export const users = sqliteTable(
         password: text("password").notNull(),
         readingMission: text("reading_mission").notNull(),
 
+        createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+        updatedAt: text("updated_at")
+            .default(sql`CURRENT_TIMESTAMP`),
+    }
+);
+
+export const learningContents = sqliteTable(
+    "learning_contents",
+    {
+        id: text("id").primaryKey(),
+        userId: text("user_id")
+            .notNull()
+            .references(() => users.id),
+        title: text("title").notNull(),
+        totalPage: integer("total_page").notNull().default(1),
+        currentPage: integer("current_page").notNull().default(1),
+        note: text("note").notNull().default(''),
         createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
         updatedAt: text("updated_at")
             .default(sql`CURRENT_TIMESTAMP`),
