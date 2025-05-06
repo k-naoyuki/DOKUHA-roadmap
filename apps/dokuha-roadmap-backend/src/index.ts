f9aa11fdf67468c85fb10ac57aef9257c04086a1
ittyi <vioiv.k4@gmail.com>
2025-05-06 09:23:21 +0900

    ✨ feat: API create learning content の実装完了

--- apps/dokuha-roadmap-backend/src/index.ts
+++ apps/dokuha-roadmap-backend/src/index.ts
@@ -1,9 +1,12 @@
 import { D1Database } from '@cloudflare/workers-types';
 import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
-import { users } from "../schema";
+import { users, learningContents } from "../schema";
 import { createUser } from "./users";
 import { DuplicateEmailError } from './errors';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateLearningContent } from '../types/learning-contents';
+import { z } from 'zod';
 
 type Bindings = {
   productionDB: D1Database;
@@ -46,6 +49,56 @@ app.post("/users", async (c) => {
       { success: false, error: 'Internal server error' },
       { status: 500 }
     );
+  }
+});
+
+/*****************************************
+ * create learning content
+ *****************************************/
+const createLearningContentSchema = z.object({
+  userId: z.string().uuid(),
+  title: z.string().min(1).max(255),
+  totalPage: z.number().min(1),
+  currentPage: z.number().min(1).optional(),
+  note: z.string().optional(),
+});
+
+app.post("/learning-contents", async (c) => {
+  const db = drizzle(c.env.productionDB);
+  
+  try {
+    const json = await c.req.json();
+    const validatedData = createLearningContentSchema.parse(json);
+
+    const newLearningContent: CreateLearningContent = {
+      ...validatedData,
+      currentPage: validatedData.currentPage || 1,
+      note: validatedData.note || '',
+    };
+
+    await db.insert(learningContents).values({
+      id: uuidv4(),
+      ...newLearningContent,
+    });
+
+    return c.json({ 
+      success: true,
+      message: 'Learning content created successfully'
+    }, { status: 201 });
+
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return c.json({ 
+        success: false, 
+        error: error.errors 
+      }, { status: 400 });
+    }
+    
+    console.error('Unexpected error:', error);
+    return c.json({ 
+      success: false, 
+      error: 'Internal server error' 
+    }, { status: 500 });
   }
 });
 
