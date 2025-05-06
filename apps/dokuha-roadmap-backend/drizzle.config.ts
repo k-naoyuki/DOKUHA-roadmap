e6bda76648d82b3bd1939f095c60cd779f662fde
ittyi <vioiv.k4@gmail.com>
2025-05-06 09:20:24 +0900

    🔧 add: drizzle-kit studioによって、GUIでローカルDBの状態を確認できるようにした

--- apps/dokuha-roadmap-backend/drizzle.config.ts
+++ apps/dokuha-roadmap-backend/drizzle.config.ts
@@ -4,4 +4,7 @@ export default {
   schema: "./schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
+  dbCredentials: {
+    url: "./.wrangler/state/v3/d1/miniflare-D1DatabaseObject/1b52ece63023367627078964169e02f25e79862c5fb40a72236be4f7aebd8b9e.sqlite",
+  },
 } satisfies Config;
