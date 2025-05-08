13852ce2b13a2dcc31178769b6e2cfedeccafd03
ittyi <vioiv.k4@gmail.com>
2025-05-09 00:41:03 +0900

    code clean
    Co-authored-by: myo-i <myo-i@users.noreply.github.com>

--- apps/dokuha-roadmap-backend/drizzle.config.ts
+++ apps/dokuha-roadmap-backend/drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
-    // NOTE: 開発用の D1_PRODUCTION_DB_ID という命名でローカル DB を作成した場合、以下の名前で .sqlite ファイルが作成される
+    // NOTE: 開発用の D1_PRODUCTION_DB_ID という id でローカル DB を作成した場合、以下の名前で .sqlite ファイルが作成される
     url: "./.wrangler/state/v3/d1/miniflare-D1DatabaseObject/452ecafc4500a35827af44758854d2db3122cb82afaa35f19f9834426ee79073.sqlite",
   },
 } satisfies Config;
