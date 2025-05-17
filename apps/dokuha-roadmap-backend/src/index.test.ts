e6142a48a480acafb006a5b473dd2209cf7848d3
ittyi <vioiv.k4@gmail.com>
2025-05-17 16:43:54 +0900

    🚧 Error: No test found in suite GET / まで

---
+++ apps/dokuha-roadmap-backend/src/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, beforeAll, afterAll, it, expect } from "vitest";
+
+describe("GET /", () => {
+	
+
+	// it("should return Hello Hono! for the root path", async () => {
+  //   if (!worker) throw new Error("Worker did not initialize"); // workerの存在確認
+  //   const resp = await worker.fetch("/");
+  //   expect(resp.status).toBe(200);
+  //   const text = await resp.text();
+  //   expect(text).toBe("Hello Hono!"); // src/index.ts で定義されているレスポンス
+  // });
+});
