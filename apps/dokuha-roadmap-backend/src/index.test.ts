4366217da48b1e2355d1dac87b69b8aeca6c0283
ittyi <vioiv.k4@gmail.com>
2025-05-17 16:51:53 +0900

    ✅ test: 最小のAPI ハロワでAPIテストができるようにした

--- apps/dokuha-roadmap-backend/src/index.test.ts
+++ apps/dokuha-roadmap-backend/src/index.test.ts
@@ -1,13 +1,10 @@
-import { describe, beforeAll, afterAll, it, expect } from "vitest";
+import { SELF } from "cloudflare:test";
+import { describe, it, expect } from "vitest";
 
-describe("GET /", () => {
-	
-
-	// it("should return Hello Hono! for the root path", async () => {
-  //   if (!worker) throw new Error("Worker did not initialize"); // workerの存在確認
-  //   const resp = await worker.fetch("/");
-  //   expect(resp.status).toBe(200);
-  //   const text = await resp.text();
-  //   expect(text).toBe("Hello Hono!"); // src/index.ts で定義されているレスポンス
-  // });
+describe("GET / Hello Hono", () => {
+	it("should return 200 and 'Hello Hono!'", async () => {
+		const response = await SELF.fetch("http://localhost:8787/");
+		expect(response.status).toBe(200);
+		expect(await response.text()).toBe("Hello Hono!");
+	});
 });
