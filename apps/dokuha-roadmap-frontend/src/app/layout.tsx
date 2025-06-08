4fe3a787cbc63c6f6297a6885b2850d897fb57c9
ittyi <vioiv.k4@gmail.com>
2025-06-08 10:25:29 +0900

    ✨ feat: clerk によるsign in、sign up、sign outの実装完了

--- apps/dokuha-roadmap-frontend/src/app/layout.tsx
+++ apps/dokuha-roadmap-frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 import Header  from "@/components/layouts/Header";
 
@@ -24,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header />
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- apps/dokuha-roadmap-frontend/src/components/layouts/Header.tsx
+++ apps/dokuha-roadmap-frontend/src/components/layouts/Header.tsx
@@ -1,7 +1,22 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 export default function Header(){
   return (
     <header>
       <h1>これはHeaderです</h1>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </header>
   )
 }
